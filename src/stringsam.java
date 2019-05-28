cd7ab7c2dd950f88d674755d0b937707d00e2095
753813 <753813@123-PC07.student.Tor.Loc>
2019-05-28 15:21:07 -0400

    1,2,3

--- src/stringsam.java
+++ src/stringsam.java
@@ -3,7 +3,24 @@ public class stringsam {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// String examples:
+				/// --------------------
+				// 1. Create a string
+				String name = "peter sigurdson";
+				System.out.println(name);
+				
+				// 2. Number of characters in the string
+				int numChars = name.length();
+				System.out.println("Number of characters: " + numChars);
+				
+				// 3. Get a specific character
+				char charAtPosition4 = name.charAt(4);
+				System.out.println("Char at Pos 4: " + charAtPosition4);
+				
+				// 4. Get a substring
+				// 5. Check if one string is equal to another
+				// 6. Make everything uppercase
+				// 7. Make everything lowercase
 	}
 
 }
