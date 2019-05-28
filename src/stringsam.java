6e207e0110b23f763014e75cb163f2149c5054fc
753813 <753813@123-PC07.student.Tor.Loc>
2019-05-28 15:24:05 -0400

    substring

--- src/stringsam.java
+++ src/stringsam.java
@@ -18,6 +18,10 @@ public class stringsam {
 				System.out.println("Char at Pos 4: " + charAtPosition4);
 				
 				// 4. Get a substring
+				String sub = name.substring(0,5);
+				System.out.println("Get substring "+sub);
+				String sub2 = name.substring(7);
+				System.out.println("Get substring "+sub2);
 				// 5. Check if one string is equal to another
 				// 6. Make everything uppercase
 				// 7. Make everything lowercase
